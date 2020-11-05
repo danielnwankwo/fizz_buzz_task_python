1574e15387449857e8bbf9c1f868117fe084338b
Daniel Nwankwo <chino@Daniels-MacBook-Air.local>
2020-11-05 22:00:43 +0000

    fizzbuzz_docs

--- fizzbuzz_task.py
+++ fizzbuzz_task.py
@@ -1,11 +1,14 @@
 
 
-
+# create the fizzbuzz class
 class fizzbuzz:
-    def __init__(self, fizz, buzz):
+    def __init__(self, fizz, buzz): # initialise the class + pass through arguments for fizz and buzz and fizzbuzz
         self.fizz = fizz
         self.buzz = buzz
-        self.fizzbuzz = fizz * buzz
+        self.fizzbuzz = fizz * buzz # if fizz and buzz are 3 and 5 then fizz x buzz is 15 therefore the loop will work for mutiples of 15
+
+# create a function which can take an empty list and pass the list of numbers 1-100 as per the task
+# for each point in the loop it will either append a number with fizz or buzz or fizzbuzz if it is a multiple of 3,5 or both respectively
 
     def fizz_buzz(self):
 
@@ -25,8 +28,8 @@ class fizzbuzz:
                 counter.append(num)
         return counter
 
-three_five = fizzbuzz(3, 5)
-print(three_five.fizz_buzz())
+three_five = fizzbuzz(3, 5) # create the object for the class and define the multiples we use in the order they come in the initialising statement
+print(three_five.fizz_buzz()) # print results
 
 
 
