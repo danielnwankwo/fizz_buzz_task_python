b4721de57b5575c65bd97a3726767c298c99990b
Daniel Nwankwo <chino@Daniels-MacBook-Air.local>
2020-11-05 21:51:13 +0000

    fizzbuzz_complete

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# fizz buzz take
+# fizz buzz game
 ## create a loop with a range of 1-100
 ## implement classes method
+
+# Task 
+## core tasks
+- Write a program that prints the numbers from 1 to 100
+- For multiples of three print "Fizz" instead of the number
+- For the multiples of five print "Buzz" instead of the number
+- For numbers which are multiples of both three and five print "FizzBuzz"
+
+## extra
+
+- make a new fizzbuzz file and make it functional
+- make it so we we can decide which numbers to substitute for fizz and buzz using functions
+- Hint: loop, range, control flow
+
+# Acceptance Criteria 
+
+- All core task are done
+- Code works with no error 
--- fizzbuzz_task.py
+++ fizzbuzz_task.py
@@ -1,26 +1,34 @@
 
 
-for num in range(1, 101):
-
-    if num % 3 == 0 and num % 5 == 0:
-        print("fizzbuzz")
-
-    elif num % 3 == 0:
-        print("fizz")
-
-    elif num % 5 == 0:
-        print("buzz")
-
-    else:
-        print(num)
-
-
-
-
 
 class fizzbuzz:
     def __init__(self, fizz, buzz):
         self.fizz = fizz
         self.buzz = buzz
-        self.fizzbuzz = fizz + "" + buzz
+        self.fizzbuzz = fizz * buzz
+
+    def fizz_buzz(self):
+
+        counter = []
+
+
+        for num in range(1, 101):
+            if num % self.fizzbuzz == 0:
+                counter.append("fizzbuzz")
+
+            elif num % self.fizz == 0:
+                counter.append("fizz")
+
+            elif num % self.buzz == 0:
+                counter.append("buzz")
+            else:
+                counter.append(num)
+        return counter
+
+three_five = fizzbuzz(3, 5)
+print(three_five.fizz_buzz())
+
+
+
+
 
